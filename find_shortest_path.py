38965918077408a226168c02d99c8a0e478d26d1
Roshan Aji Cherian <roshanaji777@gmail.com>
2023-02-18 17:28:43 +0530

    Added documentation for the code

--- find_shortest_path.py
+++ find_shortest_path.py
@@ -1,14 +1,15 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 shortestPathList = []
-# TODO: Try to make demands_dict global
 
 def find_demand(start,end,demands_dict):
+    # Simple function to find the demand value given a source and destination
     for key, demand_vector in demands_dict.items():
         if demand_vector["source"] == start and demand_vector["target"] == end:
             return demand_vector["demand_value"]
         
 def compute_total_flow(u,v, shortestPathList, demands_dict):
+    # Function to compute the total flow through each link in the graph
     total_flow = 0
     for i in shortestPathList:
         temp = i['pathList']
@@ -22,10 +23,13 @@ def find_shortest(demands_dict, graph):
     for key, demand_vector in demands_dict.items():
         source = demand_vector["source"]
         target = demand_vector["target"]
+        # Calculate the shortest path from the source to destination using dijkstra's algorithm
+        # The path list is obtained using the function
         path_list = nx.dijkstra_path(graph,source, target)
         shortestPathList.append({"source" : source,
                                  "target" : target,
                                  "pathList" : path_list})
+        # Add the path_list to demands_dict
         demand_vector["pathList"] = path_list
     for u,v,d in graph.edges(data = True):
         # if u,v is a part of the shortestpathist of any demand then add that the total flow of u,v
--- root.py
+++ root.py
@@ -1,7 +1,9 @@
 import sndlib_model, find_shortest_path, os, find_Z_value
+# Creting graph and demands_dict
 demands_dict, graph = sndlib_model.create_graph(f"{os.getcwd()}\\input\\pdh.txt")
-# print("Demands Dictionary : ", demands_dict)
 # print(demands_dict,graph)
+# Finding the shortest path and calculating total flow 
 demands_dict, graph =  find_shortest_path.find_shortest(demands_dict,graph)
+# Finding zValue for each edge within the graph
 graph = find_Z_value.find_Z_value(graph)
 print(graph.edges(data = True))
