066208e1578200f44ffd2cf63acc6444142f23ca
Roshan Aji Cherian <roshanaji777@gmail.com>
2023-02-22 19:42:44 +0530

    Added debugging and graph view in root

--- root.py
+++ root.py
@@ -7,7 +7,7 @@ demands_dict, graph = sndlib_model.create_graph(f"{os.getcwd()}\\input\\pdh.txt"
 # Finding the shortest path and calculating total flow 
 demands_dict, graph =  find_shortest_path.find_shortest(demands_dict,graph)
 # Finding zValue for each edge within the graph
-graph = find_Z_value.find_Z_value(graph)
 nx.draw_spring(graph,with_labels = True)
 plt.show()
+graph = find_Z_value.find_Z_value(graph)
 print(graph.edges(data = True))
