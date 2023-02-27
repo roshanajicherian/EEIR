445963b3000de64d6b350ff7e6e0a6c5287ed7ca
Roshan Aji Cherian <roshanajicherian@Roshans-MacBook-Air.local>
2023-02-27 06:27:49 +0530

    Modified path of input file

--- root.py
+++ root.py
@@ -2,7 +2,7 @@ import sndlib_model, find_shortest_path, os, find_Z_value
 import networkx as nx
 import matplotlib.pyplot as plt
 # Creting graph and demands_dict
-demands_dict, graph = sndlib_model.create_graph(f"{os.getcwd()}\\input\\pdh.txt")
+demands_dict, graph = sndlib_model.create_graph(f"{os.getcwd()}/input/pdh.txt")
 # print(demands_dict,graph)
 # Finding the shortest path and calculating total flow 
 demands_dict, graph =  find_shortest_path.find_shortest(demands_dict,graph)
